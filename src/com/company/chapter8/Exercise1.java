2b16fea4cfa6b3ed8a7fd2950aa29d1a1715efe9
alla2297 <alla2297@edu.easj.dk>
2021-09-27 12:40:56 +0200

    har lavet opgave 1 chapter 8

--- src/com/company/chapter8/Exercise1.java
+++ src/com/company/chapter8/Exercise1.java
@@ -1,6 +1,24 @@
 package com.company.chapter8;
 
 public class Exercise1 {
+    public static void main (String[] args){
+        countdown(100);
+    }
 
+    public static void countdown(int n) {
+        if (n == 0) {
+            System.out.println("No bottles of beer on the wall,");
+            System.out.println("no bottles of beer,");
+            System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
+            System.out.println("’cause there are no more bottles of beer on the wall!");
+
+        } else {
+            System.out.println(n+" bottles of beer on the wall,");
+            System.out.println(n+" bottles of beer,");
+            System.out.println("ya’ take one down, ya’ pass it around,");
+            System.out.println((n-1)+" bottles of beer on the wall.\n");
+            countdown(n - 1);
+        }
+    }
 }
 
